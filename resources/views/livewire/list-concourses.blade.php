b80d27c79148d034c7e312f447da5afedb4e57c4
XirTech <john.darlucio022@gmail.com>
2024-09-20 21:14:46 +0800

    Update list-concourses.blade.php

--- resources/views/livewire/list-concourses.blade.php
+++ resources/views/livewire/list-concourses.blade.php
@@ -1,5 +1,4 @@
 <div class="wrapper w-full md:max-w-5xl mx-auto pt-20 px-4">
-    <h1 class="text-xl font-medium">Available Concourses</h1>
     <section class="pt-4">
         {{ $this->table }}
     </section>
