a52c54b8a136c89e495a8e7ac89949cedce22b56
XirTech <john.darlucio022@gmail.com>
2024-09-22 14:34:10 +0800

    Redirecting to table after creation

--- app/Filament/Admin/Resources/ConcourseRateResource/Pages/CreateConcourseRate.php
+++ app/Filament/Admin/Resources/ConcourseRateResource/Pages/CreateConcourseRate.php
@@ -9,4 +9,9 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateConcourseRate extends CreateRecord
 {
     protected static string $resource = ConcourseRateResource::class;
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 }
--- app/Filament/Admin/Resources/ConcourseResource/Pages/CreateConcourse.php
+++ app/Filament/Admin/Resources/ConcourseResource/Pages/CreateConcourse.php
@@ -5,8 +5,14 @@ namespace App\Filament\Admin\Resources\ConcourseResource\Pages;
 use App\Filament\Admin\Resources\ConcourseResource;
 use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
+use Filament\Notifications\Notification;
 
 class CreateConcourse extends CreateRecord
 {
     protected static string $resource = ConcourseResource::class;
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 }
