b7c7e89f638f18d1c29f623410f612dff1d4d71d
XirTech <john.darlucio022@gmail.com>
2024-10-21 04:02:13 +0800

    Graphs

---
+++ app/Filament/Admin/Widgets/CustomerChart.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Filament\Admin\Widgets;
+
+use Leandrocfe\FilamentApexCharts\Widgets\ApexChartWidget;
+
+class CustomerChart extends ApexChartWidget
+{
+    /**
+     * Chart Id
+     *
+     * @var string
+     */
+    protected static ?string $chartId = 'customerChart';
+
+    /**
+     * Sort
+     */
+    protected static ?int $sort = 2;
+
+    /**
+     * Widget content height
+     */
+    protected static ?int $contentHeight = 270;
+
+    /**
+     * Widget Title
+     *
+     * @var string|null
+     */
+    protected static ?string $heading = 'Customer Chart';
+
+    /**
+     * Chart options (series, labels, types, size, animations...)
+     * https://apexcharts.com/docs/options
+     *
+     * @return array
+     */
+
+    protected function getOptions(): array
+    {
+        return [
+            'chart' => [
+                'type' => 'line',
+                'height' => 300,
+            ],
+            'series' => [
+                [
+                    'name' => 'CustomerChart',
+                    'data' => [2, 4, 6, 10, 14, 7, 2, 9, 10, 15, 13, 18],
+                ],
+            ],
+            'xaxis' => [
+                'categories' => ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+                'labels' => [
+                    'style' => [
+                        'fontFamily' => 'inherit',
+                    ],
+                ],
+            ],
+            'yaxis' => [
+                'labels' => [
+                    'style' => [
+                        'fontFamily' => 'inherit',
+                    ],
+                ],
+            ],
+            'colors' => ['#f59e0b'],
+            'stroke' => [
+                'curve' => 'smooth',
+            ],
+        ];
+    }
+}
---
+++ app/Filament/Admin/Widgets/StatsOverview.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Filament\Admin\Widgets;
+
+use Filament\Widgets\StatsOverviewWidget as BaseWidget;
+use Filament\Widgets\StatsOverviewWidget\Stat;
+
+class StatsOverview extends BaseWidget
+{
+    /**
+     * Sort
+     */
+    protected static ?int $sort = 1;
+
+    /**
+     * Widget content height
+     */
+    protected static ?int $contentHeight = 270;
+
+
+    protected function getStats(): array
+    {
+        return [
+            Stat::make('Unique views', '192.1k'),
+            Stat::make('Bounce rate', '21%'),
+            Stat::make('Average time on page', '3:12'),
+        ];
+    }
+}
