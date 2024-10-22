5c952fb1861f58f9144bedf9fa64fb2ffc649ee1
XirTech <john.darlucio022@gmail.com>
2024-10-22 08:03:49 +0800

    clean db

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -29,9 +29,9 @@ class UserSeeder extends Seeder
             'email_verified_at' => now(),
         ]);
 
-        // Create 500 fake users
-        User::factory()->count(500)->create()->each(function ($user) {
-            $user->assignRole('panel_user');
-        });
+        // // Create 500 fake users
+        // User::factory()->count(500)->create()->each(function ($user) {
+        //     $user->assignRole('panel_user');
+        // });
     }
 }
