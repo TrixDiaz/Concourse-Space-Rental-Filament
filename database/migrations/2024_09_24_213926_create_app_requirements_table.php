e4b4f00c8657d1f86790e3e39fe37d602716342c
XirTech <john.darlucio022@gmail.com>
2024-11-03 17:31:29 +0800

    ticket

--- database/migrations/2024_09_24_213926_create_app_requirements_table.php
+++ database/migrations/2024_09_24_213926_create_app_requirements_table.php
@@ -21,6 +21,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('file')->nullable();
             $table->string('status');
+            $table->softDeletes();
             $table->timestamps();
         });
     }
