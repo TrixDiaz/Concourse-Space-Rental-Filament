ecec343a59693dc0abdbcccda9148d06ce235574
XirTech <john.darlucio022@gmail.com>
2024-11-07 10:58:37 +0800

    seeder

--- database/seeders/ConcourseSeeder.php
+++ database/seeders/ConcourseSeeder.php
@@ -15,18 +15,24 @@ class ConcourseSeeder extends Seeder
      */
     public function run(): void
     {
-        Concourse::create([
-            'rate_id' => ConcourseRate::select('id')->inRandomOrder()->first()->id,
-            'name' => 'PUP - Main Concourse',
-            'address' => 'General Luna St. , Sampaloc, Manila, 1003 Metro Manila',
-            'spaces' => Space::select('concourse_id')->count(),
-            'image' => 'https://placehold.co/600x400',
-            'layout' => 'https://placehold.co/600x400',
-            'lease_term' => rand(1, 10),
-            'water_bills' => rand(1000, 10000),
-            'electricity_bills' => rand(1000, 10000),
-            'created_at' => now(),
-            'updated_at' => now(),
-        ]);
+        $months = range(1, 11); // January to November
+        
+        foreach ($months as $month) {
+            $date = now()->setMonth($month)->setDay(1);
+            
+            Concourse::create([
+                'rate_id' => ConcourseRate::select('id')->inRandomOrder()->first()->id,
+                'name' => 'PUP - Main Concourse ' . $month,
+                'address' => 'General Luna St. , Sampaloc, Manila, 1003 Metro Manila',
+                'spaces' => Space::select('concourse_id')->count(),
+                'image' => 'https://placehold.co/600x400',
+                'layout' => 'https://placehold.co/600x400',
+                'lease_term' => rand(1, 10),
+                'water_bills' => rand(1000, 10000),
+                'electricity_bills' => rand(1000, 10000),
+                'created_at' => $date,
+                'updated_at' => $date,
+            ]);
+        }
     }
 }
