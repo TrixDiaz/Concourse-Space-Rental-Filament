b2bda9077f26102106898cbe06b791f31fe19532
XirTech <john.darlucio022@gmail.com>
2024-11-12 19:48:20 +0800

    redo

--- resources/views/emails/application_approve.blade.php
+++ resources/views/emails/application_approve.blade.php
@@ -8,10 +8,9 @@
 </head>
 
 <body>
-    <p>{{ $greeting }}</p>
-    <p>{{ $line1 }}</p>
-    <p>{{ $line2 }}</p>
-    <p>{{ $salutation }}<br>{{ $fromName }}</p>
+    <p>We have received your security deposit, and with the contract signed by both you and the owner, your application for {{ $spaceName }} in {{ $concourseName }} at {{ $concourseAddress }} has been approved. You may now begin operating in your space.</p>
+    <p>We are delighted to have you as our tenant.</p>
+    <p>Regards,<br>COMS</p>
 </body>
 
 </html>
